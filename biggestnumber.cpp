204021ffb3d541f37fee193cf344bfe52dfed6e6
aliakbar1 <aakbar2@mail.csuchico.edu>
2018-09-10 17:22:55 -0700

    shows bigger number

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -4,19 +4,27 @@
 int main()
 {
   //declare variables here
-
+ int young;
+ int old;        
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
-
+  std::cin>>young; 
   std::cout<<"Please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
+ std::cin>>old;
 
   //determine if first or second is bigger and store that in a third variable
+if ( young<old)
+{
+        std::cout<<old;
+}
+else
 
-
-  std::cout<<"Of those two numbers, the biggest is: ";
+{
+std::cout<<young;
   //print out the variable representing the biggest number
-
+  
+}  
   std::cout<<std::endl<<"Thank you for playing.\n";
   
   return 0;
